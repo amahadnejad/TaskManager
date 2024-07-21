7ff62b5486e02a9184d0f3a705183ac3650da8e7
amahadnezhad <amahadnezhad@gmail.com>
2024-07-22 01:45:49 +0330

    Add task_create.html & task_update.html Template

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     # Third-Party Apps
     'allauth',
     'allauth.account',
+    'crispy_forms',
+    "crispy_bootstrap4",
 
     # Local Apps
     'accounts.apps.AccountsConfig',
@@ -133,3 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "accounts.CustomUser"
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# Crispy Forms Setting
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
