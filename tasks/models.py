c70d497fee234310c6b454b9c174c97919682c63
amahadnezhad <amahadnezhad@gmail.com>
2024-07-21 18:24:17 +0330

    Add Task Model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=600)
+    status = models.BooleanField(default=True)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user}: {self.title}"
