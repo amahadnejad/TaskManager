ac911fa1139f4586bd5c6f81e67a4fdf753a3270
amahadnezhad <amahadnezhad@gmail.com>
2024-07-22 01:12:04 +0330

    Add task_create_view

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import task_list_view, task_update_view, task_delete_view
+from .views import task_list_view, task_update_view, task_delete_view, task_create_view
 
 urlpatterns = [
     path('tasks/', task_list_view, name='task_list'),
+    path('task/add/', task_create_view, name='task_create'),
     path('task/<int:pk>/edit/', task_update_view, name='task_update'),
     path('task/<int:pk>/delete/', task_delete_view, name='task_delete'),
 
--- tasks/views.py
+++ tasks/views.py
@@ -16,6 +16,21 @@ def task_list_view(request):
 
 
 @login_required
+def task_create_view(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+
+    return render(request, 'tasks/task_create.html', {'form': form})
+
+
+@login_required
 def task_update_view(request, pk):
     task = get_object_or_404(Task, pk=pk, user=request.user)
 
