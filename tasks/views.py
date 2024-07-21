65d011781d294dd1f4dedf24662e7055f692841c
amahadnezhad <amahadnezhad@gmail.com>
2024-07-21 18:49:32 +0330

    Add login_required Decorator

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from .models import Task
 
 
+@login_required
 def task_list_view(request):
     # Get the tasks for the logged-in user
     tasks = Task.objects.filter(user=request.user)
