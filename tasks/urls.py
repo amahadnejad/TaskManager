c76d599f81e7da4a387d0d44d1732332b4a55339
amahadnezhad <amahadnezhad@gmail.com>
2024-07-21 19:08:57 +0330

    Add task_delete_view

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import task_list_view, task_update_view
+from .views import task_list_view, task_update_view, task_delete_view
 
 urlpatterns = [
     path('tasks/', task_list_view, name='task_list'),
     path('task/<int:pk>/edit/', task_update_view, name='task_update'),
+    path('task/<int:pk>/delete/', task_delete_view, name='task_delete'),
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -28,3 +28,14 @@ def task_update_view(request, pk):
         form = TaskForm(instance=task)
 
     return render(request, 'tasks/task_update.html', {'form': form})
+
+
+@login_required
+def task_delete_view(request, pk):
+    task = get_object_or_404(Task, pk=pk, user=request.user)
+
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+
+    return render(request, 'tasks/task_delete.html', {'task': task})
